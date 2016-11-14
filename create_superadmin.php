d0537bab9562ca3c5c90ed2033b1bc9c26d97e40
rach.ep <rachmadpribadi@hotmail.com>
2016-11-14 20:01:25 +0700

    create superadmin

--- create_superadmin.php
+++ create_superadmin.php
@@ -11,19 +11,19 @@ $username=$_POST['username'];
 $password=$_POST['password'];
 $password1=$_POST['password1'];
 $password2=$_POST['password2'];
-$password3=$_POST['password3'];
+// $password3=$_POST['password3'];
 // To protect MySQL injection for Security purpose
 $username = stripslashes($username);
 $password = stripslashes($password);
 $password1 = stripslashes($password1);
 $password2 = stripslashes($password2);
-$password3 = stripslashes($password3);
+// $password3 = stripslashes($password3);
 $username = mysqli_real_escape_string($conn, $username);
 $password = mysqli_real_escape_string($conn, $password);
 $password1 = mysqli_real_escape_string($conn, $password1);
 $password2 = mysqli_real_escape_string($conn, $password2);
-$password3 = mysqli_real_escape_string($conn, $password3);
-$passwords = $password.''.$password1.''.$password2.''.$password3;
+// $password3 = mysqli_real_escape_string($conn, $password3);
+$passwords = $password.''.$password1.''.$password2;
 $query = mysqli_query($conn, "INSERT INTO admin(username_admin, password_admin, level) VALUES ('$username',PASSWORD('$passwords'),'superadmin')");
 header("location: user.php");
 ?>
